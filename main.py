88161f7fcc4b993edc7dfdaac20f713429bba350
kanya01 <kanyamosesm@gmail.com>
2023-10-15 02:05:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import eel
+import logic.todo_controller
 
 eel.init('web')
 
-@eel.expose
-def get_data():
-    return 'got data from py'
+#@eel.expose
+#def get_data():
+#    return 'got data from py'
 
-@eel.expose
-def send_data(msg):
-    print (msg)
+#@eel.expose
+#def send_data(msg):
+ #   print (msg)
 
 eel.start('index.html')
