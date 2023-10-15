082007c4aa8c48c78e3159c1c4d7b1ca01452fd0
Moses.Mwangi <moses.mwangi@thetrainline.com>
2023-10-15 02:02:35 +0100

    further functionalitis improvement

---
+++ logic/todo_controller.py
@@ -0,0 +1,13 @@
+import eel
+
+from data.todo import Todo
+
+todo_app = Todo()
+
+@eel.expose
+def add(task):
+    todo_app.add(task)
+
+@eel.expose
+def delete(task):
+    todo_app.delete(task)
--- web/scripts/index.js
+++ web/scripts/index.js
@@ -1,11 +1,17 @@
 async function getDataFromPython(){
     document.getElementById('myele').innerText = await eel.get_data()();
 }
+async function getTasks() {
+    const tasks = await eel.get_tasks()()
+    const divTasks = document.getElementById('tasks')
+}
 
-document.getELementById('mybtn').addEventListener('click', () => {
-    getDataFromPython();
+document.getELementById('mybtn').addEventListener('click', async() => {
+    await eel.add(document.getElementById('taskinp').value)
+    getTasks()
 })
 
 document.getElementById('sendbtn').addEventListener('click', async() => {
-    await eel.send_data('js success');
+    await eel.add(document.getElementById('taskinp').value)
+    getTasks()
 })
